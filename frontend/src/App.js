892eaca68f8eba5a43a388af371495a966e5cadf
vitalijtverskoj <vitalijtverskoj@gmail.com>
2022-11-18 23:29:33 +0300

    edit App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-      axios.get('http://127.0.0.1:8000/api/users/').then(response =>{
+      axios.get('http://127.0.0.1:8000/api/users/').then(response => {
 
          this.setState(
         {'users':response.data}
