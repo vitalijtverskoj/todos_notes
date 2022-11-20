ac68ea01022bc39be32fca6f42640ba401a0e111
vitalijtverskoj <vitalijtverskoj@gmail.com>
2022-11-20 08:15:15 +0300

    add models project, todo

--- todo/models.py
+++ todo/models.py
@@ -7,6 +7,14 @@ class Project(models.Model):
     link = models.URLField(max_length=200)
     users = models.ManyToManyField(User)
 
+    def __str__(self):
+        return self.name
+
 
 class Todo(models.Model):
-    project =
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    deleted = models.BooleanField(default=False)
